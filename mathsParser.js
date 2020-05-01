361ef2a7607ea8791c58606728a16bd861ec0ac7
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-05-01 16:29:32 +0100

    Nested brackets and correct precedence

--- mathsParser.js
+++ mathsParser.js
@@ -1,8 +1,7 @@
 function evaluateInput(input) {
     parsedInput = parseInput(input);
     (parsedInput.includes("e") && (parsedInput = evaluateBrackets(parsedInput)))
-    summedInput = evaluateSummation(parsedInput)
-    product = evaluateProduct(summedInput)
+    product = workLeftToRight(parsedInput)
     return product
 }
 
@@ -16,42 +15,32 @@ function parseInput(input) {
   }
 
 function evaluateBrackets(input){
-    everythingInBracket = input.slice(input.indexOf("e")+1, input.indexOf("f"))
-    summedInput = evaluateSummation(everythingInBracket)
-    product = evaluateProduct(summedInput)
-    input.splice(input.indexOf("e"), input.indexOf("f")-1, product[0])
+    everythingInBracket = input.slice(input.indexOf("e")+1, input.lastIndexOf("f"));
+    (everythingInBracket.includes("e") && (everythingInBracket = evaluateBrackets(everythingInBracket)))
+    product = workLeftToRight(everythingInBracket)
+    input.splice(input.indexOf("e"), (input.lastIndexOf("f")-input.indexOf("e")+1), product[0])
     return input
 }
 
-function evaluateSummation(input){
-    let i=0;
+function workLeftToRight(input){
+    i=0
     while(i<input.length){
         if(input[i] === "a"){
             let sum = addingFunction(input[i-1], input[i+1])
-            input.splice(input[i], 3, sum)
+            input.splice(i-1, 3, sum)
         }else if(input[i] === "b"){
             let sum = subtractingFunction(input[i-1], input[i+1])
-            input.splice(input[i], 3, sum)
-        }else{
-            i++
-        }
-    };
-    return input
-}
-
-function evaluateProduct(input){
-    let i = 0;
-    while(i<input.length){
-        if(input[i] === "c"){
+            input.splice(i-1, 3, sum)
+        }else if(input[i] === "c"){
             let product = multiplyingFunction(input[i-1], input[i+1])
-            input.splice(input[i], 3, product)
+            input.splice(i-1, 3, product)
         }else if(input[i] === "d"){
             let product = dividingFunction(input[i-1], input[i+1])
-            input.splice(input[i], 3, product)
+            input.splice(i-1, 3, product)
         }else{
             i++
         }
-    };
+    }
     return input
 }
 
--- mathsParser.test.js
+++ mathsParser.test.js
@@ -2,6 +2,7 @@ const { parseInput, evaluateInput } = require('./mathsParser');
 
 const input1 = "3a2c4"
 const input2 = "32a2d2"
+const inpute = "32d2a2"
 const input3 = "500a10b66c32"
 const input4 = "3ae4c66fb32"
 const input5 = "3c4d2aee2a4c41fc4f"
@@ -32,6 +33,10 @@ test('evaluate with correct precedence', () => {
   expect(evaluatedInput.length).toBe(1)
   expect(evaluatedInput[0]).toBe(17)
 
+  evaluatedInput = evaluateInput(inpute)
+  expect(evaluatedInput.length).toBe(1)
+  expect(evaluatedInput[0]).toBe(18)
+
   evaluatedInput = evaluateInput(input3)
   expect(evaluatedInput.length).toBe(1)
   expect(evaluatedInput[0]).toBe(14208)
@@ -39,6 +44,10 @@ test('evaluate with correct precedence', () => {
   evaluatedInput = evaluateInput(input4)
   expect(evaluatedInput.length).toBe(1)
   expect(evaluatedInput[0]).toBe(235)
+
+  evaluatedInput = evaluateInput(input5)
+  expect(evaluatedInput.length).toBe(1)
+  expect(evaluatedInput[0]).toBe(990)
 })
 
 
