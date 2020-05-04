dc77f06ba38fae4428073c7781dfa0d38713abc8
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-05-04 10:17:23 +0100

    Interface asks question more than once

--- interface.js
+++ interface.js
@@ -2,15 +2,25 @@ const { evaluateInput } = require('./mathsParser');
 
 const readline = require("readline").createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    prompt: "Enter string expression to parse, or type exit to exit: "
   })
-  
-readline.question(`Enter string expression to parse `, (string) => {
-    product = evaluateInput(string)
-    if(product.length != 1){product = "Your input was in the wrong format"}
-    console.log(`${product}`)
-    readline.close()
-})
+
+readline.prompt();
+
+readline.on('line', (line) => {
+    switch (line.trim()) {
+      case 'exit':
+        readline.close()
+        break;
+      default:
+        product = evaluateInput(line)
+        if(product.length != 1){product = "Your input was in the wrong format"}
+        console.log(`${product}`)
+        break;
+    }
+    readline.prompt();
+  })
 
 readline.on("close", function() {
     process.exit(0);
